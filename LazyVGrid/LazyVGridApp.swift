25941208bfd5bee7b379756e9bc1f0bc0c3ed7ac
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-02 17:46:39 +0700

    Initial Commit

---
+++ LazyVGrid/LazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridApp.swift
+//  LazyVGrid
+//
+//  Created by Võ Thanh Sang on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
