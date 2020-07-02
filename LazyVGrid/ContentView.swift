74f7a82fb22b09661c71c9d1664e775c40a54b72
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-02 18:05:04 +0700

    2/07

--- LazyVGrid/ContentView.swift
+++ LazyVGrid/ContentView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animals = ["🎨" , "🎯", "🚑", "🚓", "🏩", "🙈", "🐭"]
+    @State private var sliderValue: CGFloat = 2
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        let coloums: [GridItem] = Array(repeating: .init(.flexible()), count: Int(self.sliderValue))
+        
+        
+        return VStack {
+            
+            Slider(value: $sliderValue, in: 2...10, step: 1)
+            Text(String(format: "%.0f hihi", self.sliderValue))
+            
+            ScrollView {
+                LazyVGrid(columns: coloums) {
+                    ForEach(self.animals, id: \.self) { (animal) in
+                        Text(animal)
+                            .font(.system(size: 100))
+                    }
+                }
+            }
+        }
+        
     }
 }
 
